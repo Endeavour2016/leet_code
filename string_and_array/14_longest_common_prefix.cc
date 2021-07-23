8053372753098f43724e62cf18cba3f5f250bdf7
Bruce <zhuliming@buaa.edu.cn>
2021-07-24 00:56:04 +0800

    Zlm temp branch (#5)
    * test git push with ssh

---
+++ string_and_array/14_longest_common_prefix.cc
@@ -0,0 +1,21 @@
+/*
+ * @Author: zlm 
+ * @Date: 2021-07-23 23:12:27 
+ * @Last Modified by: zlm
+ * @Last Modified time: 2021-07-24 00:50:00
+ */
+
+#include "../head_file.h"
+#include "../data_structure.h"
+using namespace std;
+
+/**
+ * @problem: 14. Longest Common Prefix
+ * @descr: 
+ * @method:
+ */
+
+int main() {
+  system ("pause");
+  return 0;
+}
