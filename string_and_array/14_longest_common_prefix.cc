963d4df3cede71db80337fe760287141f574ca88
zlm <zhuliming@buaa.edu.cn>
2021-07-24 00:44:36 +0800

    test git push with ssh

---
+++ string_and_array/14_longest_common_prefix.cc
@@ -0,0 +1,21 @@
+/*
+ * @Author: zlm 
+ * @Date: 2021-07-23 23:12:27 
+ * @Last Modified by:   zlm 
+ * @Last Modified time: 2021-07-23 23:12:27 
+ */
+
+#include "../head_file.h"
+#include "../data_structure.h"
+using namespace std;
+
+/**
+ * @problem:
+ * @descr:
+ * @method:
+ */
+
+int main() {
+  system ("pause");
+  return 0;
+}
